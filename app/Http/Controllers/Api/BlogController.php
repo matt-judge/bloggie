951d49171dc94219f2400ac01ab5032772238112
Matt Judge <matt.judge@electriccircus.co.uk>
2023-09-17 23:47:03 +0100

    Changed ordering of featured posts to be in ascending order

--- app/Http/Controllers/Api/BlogController.php
+++ app/Http/Controllers/Api/BlogController.php
@@ -20,7 +20,7 @@ class BlogController extends Controller
             ->where(function ($subquery) use ($now) {
                 return $subquery->where('expired_at', '>', $now)->orWhereNull('expired_at');
             })
-            ->orderBy('published_at', 'desc')
+            ->orderBy('published_at', 'asc')
             ->limit($request->get('limit', 3))
             ->get();
 
