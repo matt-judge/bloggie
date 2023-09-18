9777c8ddae7aac121b9e74418f772e682c9de8ef
Matt Judge <matt.judge@electriccircus.co.uk>
2023-09-18 09:58:17 +0100

    Added model reference for testimonials

--- app/Http/Controllers/Admin/TestimonialController.php
+++ app/Http/Controllers/Admin/TestimonialController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class TestimonialController extends Controller
